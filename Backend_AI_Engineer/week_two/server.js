bd84c5f3a99b730ff9bbb3cc9b1a710af74564c1
auxomeness <karlaustinpavia17@gmail.com>
2026-07-15 21:28:09 +0800

    Stage 4: full CRUD

--- Backend_AI_Engineer/week_two/server.js
+++ Backend_AI_Engineer/week_two/server.js
@@ -64,6 +64,62 @@ app.post('/tasks', (req, res) => {
   res.status(201).json(task);
 });
 
+app.put('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((item) => item.id === id);
+
+  if (!task) {
+    return res.status(404).json({
+      error: `Task ${req.params.id} not found`
+    });
+  }
+
+  const hasTitle = Object.prototype.hasOwnProperty.call(req.body, 'title');
+  const hasDone = Object.prototype.hasOwnProperty.call(req.body, 'done');
+
+  if (!hasTitle && !hasDone) {
+    return res.status(400).json({
+      error: 'Request body must include title and/or done'
+    });
+  }
+
+  if (hasTitle && (typeof req.body.title !== 'string' || req.body.title.trim() === '')) {
+    return res.status(400).json({
+      error: 'Title must not be empty'
+    });
+  }
+
+  if (hasDone && typeof req.body.done !== 'boolean') {
+    return res.status(400).json({
+      error: 'Done must be true or false'
+    });
+  }
+
+  if (hasTitle) {
+    task.title = req.body.title.trim();
+  }
+
+  if (hasDone) {
+    task.done = req.body.done;
+  }
+
+  res.json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const taskIndex = tasks.findIndex((item) => item.id === id);
+
+  if (taskIndex === -1) {
+    return res.status(404).json({
+      error: `Task ${req.params.id} not found`
+    });
+  }
+
+  tasks.splice(taskIndex, 1);
+  res.status(204).send();
+});
+
 app.listen(PORT, () => {
   console.log(`Task API is running on http://localhost:${PORT}`);
 });
